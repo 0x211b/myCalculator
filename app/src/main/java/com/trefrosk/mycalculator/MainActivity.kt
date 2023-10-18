5df6c076a422b0c0f527b32e77e178486e75ca2c
0x211b <lh568@pm.me>
2023-10-17 18:01:38 -0800

    first row of buttons

--- app/src/main/java/com/trefrosk/mycalculator/MainActivity.kt
+++ app/src/main/java/com/trefrosk/mycalculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.trefrosk.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(View){
+        Toast.makeText(this,"button clicked", Toast.LENGTH_LONG).show()
     }
 }
